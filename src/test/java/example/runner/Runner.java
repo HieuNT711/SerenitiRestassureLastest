26b5dfe88a30f561fe75659d6ba29a6aab75949a
nguyentrunghieu71199 <trunghieugtvt71199@gmail.com>
2024-06-01 12:46:55 +0700

    update

--- src/test/java/example/runner/Runner.java
+++ src/test/java/example/runner/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/Debug.feature", "src/test/resources/features/Debug2.feature"},
+        features = {"src/test/resources/features/Debug.feature"},
         glue = {"example.steps"},
         plugin = {"pretty"}
 //        tags = "@1"
